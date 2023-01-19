43cc871ef5dd0ee6874a051d35027570c6dbf3d1
Seungjoo Yoo <gayeul0702@gmail.com>
2023-01-20 02:16:00 +0900

    Create(DBcafe_Project):create new project and folders (#1)

---
+++ src/test/java/kr/co/dbcafe/DBcafeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dbcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBcafeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
