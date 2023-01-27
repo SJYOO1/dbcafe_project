9a0988aac0310d05a3a923e81de9e70e3e44aea4
GeonWoo Yun <kwyoon0705@gmail.com>
2023-01-27 17:02:36 +0900

    Hello This is My First Commit ^^**

--- src/main/java/kr/co/dbcafe/vo/OrdVO.java
+++ src/main/java/kr/co/dbcafe/vo/OrdVO.java
@@ -1,26 +1,25 @@
 package kr.co.dbcafe.vo;
 
 import java.util.Date;
+import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrdVO {
 	private int ordNo;
 	private String stNo;
 	private Date ordDt;
 	private char typeNo;
 	private char payNo;
-	
-	class DetailVO {
+	private List<DetailVO> detailVOs;
+
+	@Data
+	public static class DetailVO {
 		private int detailNo;
 		private int ordNo;
 		private String gdNo;
 		private int ordQn;
-		private int toPrice;
+		private int totPrice;
 	}
 }
