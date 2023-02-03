bacbdd45b964581f8c34a2700ede4ac6acab35aa
gpxu <gpxuss@gmail.com>
2023-02-03 09:21:35 +0900

    Category

---
+++ src/main/java/kr/co/dbcafe/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package kr.co.dbcafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+
+	// 폼을 띄운다.
+	@GetMapping(value = {"/categoryDate"})
+	public String index(Model model){
+		return "categoryDate";
+	}
+}
